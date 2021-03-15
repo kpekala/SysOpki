f287e71b78c418f990bdda097d694b08643e5ded
Konrad <kgpekala@gmail.com>
2021-03-15 21:22:31 +0100

    added merging raport

--- lab1/main.c
+++ lab1/main.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/times.h>
 
 #include "mylib/mylib.h"
 
 #define WORD_SIZE 30
 
+struct timespec begin, end;
+struct timespec cpu_begin, cpu_end;
+
 int main() {
-    float all_seconds = 0;
+    double all_time = 0;
+    double all_time_cpu = 0;
+
     while (1){
         char s1[WORD_SIZE];
         scanf("%s",s1);
-        clock_t  start = clock();
+        clock_gettime(CLOCK_REALTIME, &begin);
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &cpu_begin);
         if(strcmp(s1,"q") == 0)
             break;
         else if (strcmp(s1,"create_table") == 0){
@@ -47,13 +54,19 @@ int main() {
             }
 
         }
-        clock_t end = clock();
-        all_seconds += (float)(end - start) / CLOCKS_PER_SEC;
+        clock_gettime(CLOCK_REALTIME, &end);
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &cpu_end);
+        long seconds = end.tv_sec - begin.tv_sec;
+        long nanoseconds = end.tv_nsec - begin.tv_nsec;
+        long cpu_sec = cpu_end.tv_sec - cpu_begin.tv_sec;
+        long cpu_nansec = cpu_end.tv_nsec - cpu_begin.tv_nsec;
+        all_time += seconds + nanoseconds*1e-9;
+        all_time_cpu += cpu_sec + cpu_nansec*1e-9;
     }
     clean();
-    printf("Time spend: %f\n",all_seconds);
+    printf("cpu Time measured: %.3f seconds.\n", all_time_cpu);
+    printf("wall time measured: %.3f seconds.\n", all_time);
     return 0;
-
     /*Testing:
 
      */
