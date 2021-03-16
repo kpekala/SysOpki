537adb92feee8c84da248e0190f0421a70249771
Konrad <kgpekala@gmail.com>
2021-03-16 23:27:18 +0100

    added measuring of memory cleaning

--- lab1/main.c
+++ lab1/main.c
@@ -10,6 +10,20 @@
 struct timespec begin, end;
 struct timespec cpu_begin, cpu_end;
 
+void clean_data(){
+    struct timespec c_begin, c_end;
+    struct timespec c_cpu_begin, c_cpu_end;
+    clock_gettime(CLOCK_REALTIME, &begin);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &cpu_begin);
+    clean();
+    clock_gettime(CLOCK_REALTIME, &end);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &cpu_end);
+    double w_t = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec)*1e-9;
+    double cpu_t = (cpu_end.tv_sec - cpu_begin.tv_sec) + (cpu_end.tv_nsec - cpu_begin.tv_nsec)*1e-9;
+    printf("cleaning cpu Time measured: %.3f seconds.\n", cpu_t);
+    printf("cleaning wall time measured: %.3f seconds.\n", w_t);
+}
+
 int main() {
     double all_time = 0;
     double all_time_cpu = 0;
@@ -63,7 +77,8 @@ int main() {
         all_time += seconds + nanoseconds*1e-9;
         all_time_cpu += cpu_sec + cpu_nansec*1e-9;
     }
-    clean();
+
+    clean_data();
     printf("cpu Time measured: %.3f seconds.\n", all_time_cpu);
     printf("wall time measured: %.3f seconds.\n", all_time);
     return 0;
