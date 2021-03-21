95e19f6225761c107c2b950fe1d0832e20bf50eb
Konrad <kgpekala@gmail.com>
2021-03-21 13:41:34 +0100

    zestaw2: added task 2

---
+++ lab2/zad2/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_LINE_SIZE 256
+
+void do_magic(char symbol, char *f_name);
+
+int main(int argc, char ** argv) {
+    char* f_name;
+    char symbol;
+    if(argc < 3){
+        printf("Error: not enough arguments passed\n");
+        return 1;
+    }
+    symbol = *argv[1];
+    f_name = argv[2];
+
+    do_magic(symbol, f_name);
+}
+
+void do_magic(char symbol, char *f_name) {
+    FILE * fp1;
+    fp1 = fopen(f_name,"r");
+    if (fp1 == NULL){
+        printf("Cannot read file\n");
+        return;
+    }
+    char * line = calloc(MAX_LINE_SIZE, sizeof(char));
+    while (1){
+        char char_buffer;
+        //reading line to buffer
+        int hasSymbol = 0;
+        size_t size1;
+        for (int i = 0; i < MAX_LINE_SIZE; i++) {
+            size1 = fread(&char_buffer, sizeof(char), 1, fp1);
+            if (size1 == 0 || char_buffer == '\n')
+                break;
+            line[i] = char_buffer;
+            if (char_buffer == symbol)
+                hasSymbol = 1;
+        }
+        if (hasSymbol)
+            printf("%s\n",line);
+        memset(line,0,strlen(line));
+        if(size1 == 0)
+            break;
+    }
+    free(line);
+    fclose(fp1);
+}
+
