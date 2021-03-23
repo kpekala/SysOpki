f10752e5f38c30e8eba50ea5d9c128835bb95a14
Konrad <kgpekala@gmail.com>
2021-03-23 14:06:01 +0100

    zestaw2: added task 4

--- lab2/zad4/main.c
+++ lab2/zad4/main.c
@@ -8,7 +8,7 @@
 void do_magic(char f_name1[], char f_name2[], char n1[], char n2[]);
 
 int main() {
-    do_magic("f1.txt","f2.txt","abc","123");
+    do_magic("f1.txt","f2.txt","Warzone","Fornite");
     return 0;
 }
 
@@ -23,21 +23,21 @@ void do_magic(char f_name1[], char f_name2[], char n1[], char n2[]){
 
     fseek (fp1 , 0 , SEEK_END);
     long f_size = ftell (fp1);
-    printf("size %ld",f_size);
+    printf("size %ld\n",f_size);
     rewind(fp1);
     char * buffer1 = calloc(f_size, sizeof(char));
 
     size_t size1 = fread(buffer1, sizeof(char), f_size, fp1);
     for(int i=0; i<f_size; i++){
-        /*while (&buffer1[i]!= ){
-
-        }*/
+        if(&buffer1[i] != strstr(&buffer1[i],n1)){
+            fwrite(&buffer1[i], sizeof(char ),1,fp2);
+        }else{
+            fwrite(n2, sizeof(char ), strlen(n2),fp2);
+            i+= ((int)strlen(n1)-1);
+        }
     }
 
     char * p_str = strstr(buffer1,n1);
-    while (p_str != NULL){
-
-    }
 
     free(buffer1);
     fclose(fp1);
