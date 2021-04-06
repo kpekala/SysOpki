ee8060af619d87405fda9db51ba543c8a062b8cc
Konrad <kgpekala@gmail.com>
2021-04-06 17:24:10 +0200

    lab3: added most of task 3

--- lab3/zad3/main.c
+++ lab3/zad3/main.c
@@ -8,8 +8,14 @@
 #include <dirent.h>
 #include <string.h>
 
+#define PATH_SIZE 60
+#define MAX_FILE_SIZE 256
+
 int is_directory(const char *path);
 int is_text_file(const char *path);
+void search_directory(const char dir_path[PATH_SIZE], char* pattern, int depth_left);
+int is_real_directory(const char *path);
+int pattern_in_file(char * f_path,char * pattern);
 
 int main(int argc, char *argv[]) {
     // Na wejsciu dostajemy: 1. sciezke, 2. lancuch, 3. maksymalna dlugosc
@@ -20,26 +26,36 @@ int main(int argc, char *argv[]) {
     char* path = argv[1];
     char* pattern = argv[2];
     int max_depth = atoi(argv[3]);
+    search_directory(path, pattern, max_depth);
 
-    DIR* dir = opendir(path);
+    return 0;
+}
+
+void search_directory(const char dir_path[PATH_SIZE], char* pattern, int depth_left){
+    DIR* dir = opendir(dir_path);
     if (dir == NULL){
-        printf("Dir is null\n");
-        return 1;
+        printf("Dir is null, %s\n",dir_path);
+        return;
     }
 
     struct dirent* file_d = readdir(dir);
     while (file_d != NULL){
-        printf("Kolejny pliczek: %s\n",file_d->d_name);
-        char f_path[50] = "";
-        strcat(f_path,path);
+        printf("%s: %s\n",dir_path, file_d->d_name);
+        char f_path[PATH_SIZE] = "";
+        strcat(f_path,dir_path);
         strcat(f_path,file_d->d_name);
-        printf("Sciezka pliczku: %s\n",f_path);
-        printf("Fajny katalog? %d\n",is_directory(f_path));
+        if(is_directory(f_path) && is_real_directory(f_path) && depth_left > 0){
+            //bez nowego procesu narazie
+            printf("Going to %s\n",f_path);
+            strcat(f_path,"/");
+            search_directory(f_path, pattern, depth_left-1);
+        }else if(is_text_file(f_path) && pattern_in_file(f_path, pattern)){
+            printf("Pattern in %s!\n",f_path);
+        }
         file_d = readdir(dir);
     }
 
     closedir(dir);
-    return 0;
 }
 
 int is_directory(const char *path) {
@@ -53,3 +69,21 @@ int is_text_file(const char *path){
     return strstr(path,".txt") != NULL;
 }
 
+int is_real_directory(const char *path){
+    return strstr(path,"..") == NULL && path[strlen(path)-1] != '.';
+}
+
+int pattern_in_file(char * f_path, char * pattern){
+    FILE * fp1;
+    fp1 = fopen(f_path, "r");
+    if (fp1 == NULL){
+        printf("Cannot read file\n");
+        return 0;
+    }
+    char * buffer1 = calloc(MAX_FILE_SIZE, sizeof(char));
+    fread(buffer1, sizeof(char), MAX_FILE_SIZE,fp1);
+    char* result = strstr(buffer1, pattern);
+    free(buffer1);
+    fclose(fp1);
+    return result != NULL;
+}
